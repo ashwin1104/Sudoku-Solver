cd1d5ec4473b6bb42561765067fc74d6a63451fe
ashwin1104 <ashwin.saxena1@gmail.com>
2019-10-20 18:10:18 -0500

    Completed SudokuSolver class implementation

--- src/SudokuSolver.cpp
+++ src/SudokuSolver.cpp
@@ -1,6 +1,5 @@
 #include "SudokuSolver.h"
 
-
 bool SudokuSolver::IsValid(std::string &sudoku_problem) {
     if (sudoku_problem.length() != kAcceptableSudokuSize) {
         return false;
@@ -14,6 +13,89 @@ bool SudokuSolver::IsValid(std::string &sudoku_problem) {
 }
 
 std::string SudokuSolver::Solve(std::string &sudoku_problem) {
+    if (!IsValid(sudoku_problem)) {
+        return sudoku_problem;
+    }
     std::string sudoku_solution = sudoku_problem;
+    int board_index = 0;
+    if (!SolveSudoku(sudoku_solution)) {
+        return sudoku_problem;
+    }
     return sudoku_solution;
+}
+
+bool SudokuSolver::SolveSudoku(std::string sudoku_solution) {
+    int board_index;
+    if (!FindUnassignedLocation(sudoku_solution, board_index)) {
+        return true;
+    }
+    for (int num = 1; num <= 9; num++) {
+        if (IsSafe(sudoku_solution, board_index, num)) {
+            sudoku_solution[board_index] = num;
+            if (SolveSudoku(sudoku_solution)) {
+                return true;
+            }
+            sudoku_solution[board_index] = '_';
+        }
+    }
+    return false;
+}
+
+bool SudokuSolver::FindUnassignedLocation(std::string sudoku_solution, int &board_index)
+{
+    int temp_char_index = 0;
+    for (int row = 0; row < kNumCharsInRow; row++) {
+        for (int col = 0; col < kNumCharsInRow; col++) {
+            temp_char_index = (kNumCharsInRow * row) + col;
+            if (sudoku_solution[temp_char_index] == '_') {
+                board_index = (row * kNumCharsInRow) + col;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool SudokuSolver::UsedInRow(std::string sudoku_solution, int row, int num) {
+    int temp_char_index = 0;
+    for (int col = 0; col < ; col++) {
+        temp_char_index = (kNumCharsInRow * row) + col;
+        if (sudoku_solution[temp_char_index] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool SudokuSolver::UsedInCol(std::string sudoku_solution, int col, int num)
+{
+    int temp_char_index = 0;
+    for (int row = 0; row < kNumCharsInRow; row++) {
+        temp_char_index = (kNumCharsInRow * row) + col;
+        if (sudoku_solution[temp_char_index] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool SudokuSolver::UsedInBox(std::string sudoku_solution, int boxStartRow, int boxStartCol, int num)
+{
+    int temp_char_index = 0;
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            temp_char_index = (kNumCharsInRow * row) + col;
+            if (sudoku_solution[temp_char_index] == num) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool SudokuSolver::IsSafe(std::string sudoku_solution, int board_index, int num) {
+    int col = board_index % kNumCharsInRow;
+    int row = (board_index - col)/kNumCharsInRow;
+    return !UsedInRow(sudoku_solution, row, num) && !UsedInCol(sudoku_solution, col, num) &&
+           !UsedInBox(sudoku_solution, row - row % 3 , col - col % 3, num);
 }
--- src/SudokuSolver.h
+++ src/SudokuSolver.h
@@ -4,13 +4,28 @@
 #endif //SUDOKU_SUDOKUSOLVER_H
 #include <string>
 
+//https://stackoverflow.com/questions/19022739/sudoku-solver-in-c
 class SudokuSolver {
-private:
-    std::string initial_puzzle_;
-    const std::string kLegalCharacters = "123456789_";
-    const int kAcceptableSudokuSize = 81;
-public:
-    // required constructor
-    bool IsValid(std::string &sudoku_problem);
-    std::string Solve(std::string &sudoku_problem);
+    private:
+        std::string initial_puzzle_;
+        const std::string kLegalCharacters = "123456789_";
+        const int kAcceptableSudokuSize = 81;
+        const int kNumCharsInRow = 9;
+    public:
+        // Returns whether or not the sudoku board is of legal length and has legal characters
+        bool IsValid(std::string &sudoku_problem);
+        // Returns the solution string, which will be the initial sudoku board if it is unsolvable
+        std::string Solve(std::string &sudoku_problem);
+        // Assigns values to all unassigned locations for Sudoku solution
+        bool SolveSudoku(std::string sudoku_solution);
+        // Searches the grid to find an entry that is still unassigned.
+        bool FindUnassignedLocation(std::string sudoku_solution, int &board_index);
+        // Returns whether it will be legal to assign num to the given row,col location.
+        bool IsSafe(std::string sudoku_solution, int board_index, int num);
+        // Returns whether any assigned entry n the specified row matches the given number.
+        bool UsedInRow(std::string sudoku_solution, int row, int num);
+        // Returns whether any assigned entry in the specified column matches the given number.
+        bool UsedInCol(std::string sudoku_solution, int col, int num);
+        // Returns whether any assigned entry within the specified 3x3 box matches the given number
+        bool UsedInBox(std::string sudoku_solution, int boxStartRow, int boxStartCol, int num);
 };

d08e5d0c82440e32cd5076a9d07a9276045d9689
Patrick J Gallagher <patrickj@cpgallagher.com>
2019-10-20 12:36:11 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+ * Write your sudoku program! Do not put all of your code in main.cpp;
+ * make new files as necessary.
+ *
+ * Make sure that the correct .cpp and .h/.hpp files are available to the
+ * sudoku and testing executables as necessary.
+ * CLion should prompt you to add the right info to the CMakeLists.txt
+ * whenever you create new .cpp files.
+ */
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
